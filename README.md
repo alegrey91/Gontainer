f240d38e572a8ce09530112e7ed4230ae8eb9f7d
Developer <clave@10.10.10.114>
2020-02-15 17:25:39 +0100

    First release

---
+++ README.md
@@ -0,0 +1,108 @@
+# Gontainer
+
+![](./Gontainer.jpg)
+
+**Gontainer** is a container made for fun and curiosity.
+
+The scope of this project was to better understand Linux namespacing, and apply it to create a rudimental container.
+
+
+
+## Install
+
+If you have a [Go](https://golang.org/) environment ready to go, it's as easy as:
+
+```bash
+go get github.com/alegrey91/Gontainer
+```
+
+Once you retrieved you are ready to build:
+
+```bash
+go build github.com/alegrey91/Gontainer
+```
+
+
+
+## Usage
+
+Typing `Gontainer -h` the following output will be shown:
+
+```
+Usage: ./Gontainer -run -uid [-mnt=/path/rootfs] [-uts [-hst=new_hostname]] [-ipc] [-net] [-pid]
+  -mnt='/path/rootfs'   Enable Mount namespace
+  -uts                  Enable UTS namespace
+  -hst='new_hostname'   Set a custom hostname into the container
+  -ipc                  Enable IPC namespace
+  -net                  Enable Network namespace
+  -pid                  Enable PID namespace
+  -uid                  Enable User namespace
+```
+
+Below there is a full explanation of provided arguments:
+
+* **mnt**: Mount namespaces control mount points. Upon creation the mounts from the current mount namespace are copied to the new namespace. The clone flag used to create a new namespace of this type is CLONE_NEWNS. [6]
+* **uts**: UTS namespaces allow a single system to appear to have different host and domain names to different processes. The clone flag used to create a new namespace of this type is CLONE_NEWUTS. [6]
+* **ipc**: IPC namespaces isolate processes from SysV style inter-process communication. This prevents processes in different IPC namespaces from using, for  example, the SHM family of functions to establish a range of shared  memory between the two processes. The clone flag used to create a new namespace of this type is CLONE_NEWIPC. [6]
+* **net**: Network namespaces virtualize the network stack. On creation a network namespace contains only a loopback interface. The clone flag used to create a new namespace of this type is CLONE_NEWNET. [6]
+* **pid**: The PID namespace provides processes with an independent set of process IDs (PIDs) from other namespaces. The first process created in a PID namespace is assigned the process id  number 1 and receives most of the same special treatment as the normal  init process. The clone flag used to create a new namespace of this type is CLONE_NEWPID. [6]
+* **uid**: User namespaces are a feature to provide both privilege isolation and  user identification segregation across multiple sets of processes  available since kernel 3.8. With administrative assistance it is possible to build a container with  seeming administrative rights without actually giving elevated  privileges to user processes. The clone flag used to create a new namespace of this type is CLONE_NEWUSER. [6]
+
+
+
+## Examples
+
+If you are interested in understanding how a containerized process is isolated from the rest of the system, follow the next step.
+
+### User ID isolation
+
+From your terminal run:
+
+```Gontainer -run -uid```
+
+The result will be:
+
+```
+[user@real-hostname ~]$ ./Gontainer -run -uid
+[Gontainer config]
+• mnt:  ""
+• uts:  disabled
+• ipc:  disabled
+• net:  disabled
+• uid:  enabled
+
+📦 [root@real-hostname] ~/home/user ‣  
+```
+
+What's happened? 
+
+We are trying to running `Gontainer` from the home directory of a non privileged user (`user`).
+
+Using the flag option `-uid` we are mapping our local UID with the container's `root` UID.
+
+For this reason, we are `root` inside the container. First magic of Linux namespaces!
+
+### Mount isolation
+
+Commonly called as `chroot` this represents the true essence of the system isolation.
+
+First of all, we need a basic root filesystem. If you have docker installed, you can retrieve a rootfs from it:
+
+`docker container inspect alpine | grep UpperDir`
+
+Just `cp -r` the resultant path to `/tmp/rootfs` and then:
+
+`Gontainer -run -uid -mnt /tmp/rootfs`
+
+As you can see, your OS file system has disappeared, leaving space for a new file system (the alpine fs).
+
+
+
+## References
+
+1. https://medium.com/@teddyking/linux-namespaces-850489d3ccf
+2. https://medium.com/@ssttehrani/containers-from-scratch-with-golang-5276576f9909
+3. http://ifeanyi.co/posts/linux-namespaces-part-1/
+4. https://klotzandrew.com/blog/container-from-scratch
+5. https://www.infoq.com/articles/build-a-container-golang/
+6. https://en.wikipedia.org/wiki/Linux_namespaces
---
+++ main.go
@@ -0,0 +1,353 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+/*
+Common constants.
+*/
+const program_name = "Gontainer"
+const version = "0.7"
+const shell = "/bin/sh"
+
+/*
+Struct to maintain cli flags.
+*/
+type Opts struct {
+	mnt string
+	uts bool
+	hst string
+	ipc bool
+	net bool
+	pid bool
+	uid bool
+	ver bool
+	run bool
+	ns  bool
+	//	cmd string
+}
+
+func main() {
+
+	/*
+		Command line flags management
+	*/
+	opt := new(Opts)
+	flag.Usage = func() {
+		fmt.Println("Usage: ./Gontainer -run -uid [-mnt=/path/rootfs] [-uts [-hst=new_hostname]] [-ipc] [-net] [-pid]")
+		fmt.Println("  -mnt='/path/rootfs'	Enable Mount namespace")
+		fmt.Println("  -uts			Enable UTS namespace")
+		fmt.Println("  -hst='new_hostname'	Set a custom hostname into the container")
+		fmt.Println("  -ipc			Enable IPC namespace")
+		fmt.Println("  -net			Enable Network namespace")
+		fmt.Println("  -pid			Enable PID namespace")
+		fmt.Println("  -uid			Enable User namespace")
+	}
+	flag.StringVar(&opt.mnt, "mnt", "", "Enable Mount namespace")
+	flag.BoolVar(&opt.uts, "uts", false, "Enable UTS namespace")
+	flag.StringVar(&opt.hst, "hst", "", "Set Hostname [-uts option required]")
+	flag.BoolVar(&opt.ipc, "ipc", false, "Enable IPC namespace")
+	flag.BoolVar(&opt.net, "net", false, "Enable Network namespace")
+	flag.BoolVar(&opt.pid, "pid", false, "Enable Process ID namespace")
+	flag.BoolVar(&opt.uid, "uid", false, "Enable User ID namespace")
+	flag.BoolVar(&opt.run, "run", false, "Start container")
+	flag.BoolVar(&opt.ns, "ns", false, "Start container namespace")
+	//	flag.StringVar(&opt.cmd, "c", "/bin/sh", "[TODO] Execute command")
+	flag.Parse()
+
+	flag_code := gen_cloneflags(*opt)
+
+	switch os.Args[1] {
+	case "-run":
+		run(flag_code)
+	case "-ns":
+		run_with_ns(opt)
+	default:
+		//panic("Error!")
+		fmt.Println("Wrong arguments passed.")
+		os.Exit(1)
+	}
+}
+
+/*
+Function to debug cli flags values.
+*/
+func opts_debug(opt *Opts) {
+
+	enabled := "\033[1;32menabled\033[0m"
+	disabled := "\033[1;31mdisabled\033[0m"
+
+	fmt.Println("[Gontainer config]")
+	if opt.mnt != "" {
+		fmt.Printf("• mnt:  \"%v\"\n", opt.mnt)
+	} else {
+		fmt.Println("• mnt:  \"\"")
+	}
+
+	if opt.uts != false {
+		fmt.Println("• uts: ", enabled)
+		if opt.hst != "" {
+			fmt.Printf(" ↳ %s\n", opt.hst)
+		}
+	} else {
+		fmt.Println("• uts: ", disabled)
+	}
+
+	if opt.ipc != false {
+		fmt.Println("• ipc: ", enabled)
+	} else {
+		fmt.Println("• ipc: ", disabled)
+	}
+
+	if opt.net != false {
+		fmt.Println("• net: ", enabled)
+	} else {
+		fmt.Println("• net: ", disabled)
+	}
+
+	if opt.uid != false {
+		fmt.Println("• uid: ", enabled)
+	} else {
+		fmt.Println("• uid: ", disabled)
+	}
+
+	fmt.Println()
+}
+
+/*
+Setup selected namespaces.
+*/
+func run(flag_code int) {
+
+	cmd := exec.Command("/proc/self/exe", append([]string{"-ns"}, os.Args[2:]...)...)
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	// Set Namespaces with generated value
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: uintptr(flag_code),
+		UidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 0,
+				HostID:      os.Getuid(),
+				Size:        1,
+			},
+		},
+		GidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 0,
+				HostID:      os.Getgid(),
+				Size:        1,
+			},
+		},
+	}
+	cmd.Run()
+}
+
+/*
+Start containerized process using selected namespaces.
+*/
+func run_with_ns(opt *Opts) {
+
+	opts_debug(opt)
+	/*
+		Makes corresponding namespaces actions,
+		if flag was set
+	*/
+	set_mnt(opt)
+	set_uts(opt)
+	set_ipc(opt)
+	set_net(opt)
+	set_pid(opt)
+	set_uid(opt)
+
+	//cmd := exec.Command(container_cmd(opt))
+	cmd := exec.Command(shell)
+	cmd.Env = []string{"PS1=📦 [$(whoami)@$(hostname)] ~$(pwd) ‣ "}
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	cmd.Run()
+
+	unset_pid(opt)
+}
+
+/*
+Generate value to enable selected namespaces.
+The function parse the 'opt' struct to generate
+final int value using binary OR operator.
+It copies a bit if it is existing in either operand.
+*/
+func gen_cloneflags(opt Opts) int {
+	flag_code := 0
+	//	if opt.mnt != "" {
+	if _, err := os.Stat(opt.mnt); !os.IsNotExist(err) {
+		flag_code = flag_code | syscall.CLONE_NEWNS
+	}
+	if opt.uts != false {
+		flag_code = flag_code | syscall.CLONE_NEWUTS
+	}
+	if opt.ipc != false {
+		flag_code = flag_code | syscall.CLONE_NEWIPC
+	}
+	if opt.net != false {
+		flag_code = flag_code | syscall.CLONE_NEWNET
+	}
+	if opt.pid != false {
+		flag_code = flag_code | syscall.CLONE_NEWPID
+	}
+	if opt.uid != false {
+		flag_code = flag_code | syscall.CLONE_NEWUSER
+	}
+	return flag_code
+}
+
+/* TODO
+Set command to be executed inside the container instance.
+func container_cmd(opt *Opts) string {
+	cmd := ""
+	if opt.cmd != "" {
+		cmd = shell + " -c " + opt.cmd
+	} else {
+		cmd = shell
+	}
+	return cmd
+}
+*/
+
+/*
+Set MNT namespace environment, checking
+'opt' struct to retrieve passed arguments.
+Specifically, it chroot the rootfs passed
+by cli (with -hst flag).
+*/
+func set_mnt(opt *Opts) bool {
+	if _, err := os.Stat(opt.mnt); !os.IsNotExist(err) {
+		if err := syscall.Chroot(opt.mnt); err != nil {
+			fmt.Println("Error setting MNT namespace.")
+			os.Exit(1)
+		}
+		if err := syscall.Chdir("/"); err != nil {
+			fmt.Println("Error during change dir.")
+			os.Exit(1)
+		}
+	} else {
+		return false
+	}
+	return true
+}
+
+/*
+Set UTS namespace environment, checking
+'opt' struct to retrieve passed arguments.
+Specifically set the provided hostname passed
+by cli (with -hst flag), otherwise set the
+default hostname of the program.
+*/
+func set_uts(opt *Opts) bool {
+	var hostname string
+	if opt.uts != false {
+		if opt.hst != "" {
+			hostname = opt.hst
+		} else {
+			hostname = program_name
+		}
+		if err := syscall.Sethostname([]byte(hostname)); err != nil {
+			fmt.Println("Error setting UTS namespace.")
+			os.Exit(1)
+			//panic(err)
+		}
+	} else {
+		return false
+	}
+	return true
+}
+
+/* TODO
+Set IPC namespace environment, checking
+'opt' struct to retrieve passed arguments.
+*/
+func set_ipc(opt *Opts) bool {
+	if opt.ipc == false {
+		return false
+	}
+	return true
+}
+
+/* TODO
+Set NET namespace environment, checking
+'opt' struct to retrieve passed arguments.
+*/
+func set_net(opt *Opts) bool {
+	if opt.net == false {
+		return false
+	}
+	return true
+}
+
+/*
+Set PID namespace environment, checking
+'opt' struct to retrieve passed arguments.
+Specifically, it mount the 'proc' fs.
+*/
+func set_pid(opt *Opts) bool {
+	// Check if option mount was set, if not, return false
+	if opt.mnt != "" {
+		if opt.pid != false {
+			if err := syscall.Mount("proc", "proc", "proc", 0, ""); err != nil {
+				fmt.Println("Error setting PID namespace.")
+				os.Exit(1)
+			}
+			//			if err := syscall.Mount("dev", "/dev", "devtmpfs", 0, ""); err != nil {
+			//				fmt.Println("Error setting PID namespace.")
+			//				os.Exit(1)
+			//			}
+		}
+	} else {
+		if opt.pid != false {
+			fmt.Println("Error: option -pid require -mnt.")
+		}
+		return false
+	}
+	return true
+}
+
+/*
+Unset pid namespace environment.
+Umount /proc from filesystem.
+*/
+func unset_pid(opt *Opts) bool {
+	if opt.mnt != "" {
+		if opt.pid != false {
+			if err := syscall.Unmount("/proc", 0); err != nil {
+				fmt.Println("Error unsetting PID namespace.")
+				os.Exit(1)
+			}
+		}
+	} else {
+		return false
+	}
+	return true
+}
+
+/* TODO
+Set UID namespace environment, checking
+'opt' struct to retrieve passed arguments.
+Specifically, it map your uid/gid, with the
+container uid/gid.
+*/
+func set_uid(opt *Opts) bool {
+	if opt.uid == false {
+		return false
+	}
+	return true
+}
